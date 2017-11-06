512f98763d19df81215b51c078a06c355af88b9d
Nesrine <nesrinakon@gmail.com>
2017-11-06 09:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay( array1 , array2){
   let resultArray = []
   for( let i =0; i<array1.length; i++){
-    resultArray[i] = `"${array1[i]} plays ${array2[i]}"`
+    resultArray[i] = `${array1[i]} plays ${array2[i]}`
   }
   return resultArray
 }
